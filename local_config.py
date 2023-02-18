4980510b2a3c394d6bb13ac4a020526daae956a7
Colt Mercer <42218163+ColtMercer@users.noreply.github.com>
2023-02-18 14:18:20 -0600

    Update local_config.py

--- local_config.py
+++ local_config.py
@@ -1,16 +1,16 @@
-PLUGINS = ["my_plugin"]
+PLUGINS = ["ca_instance"]
 
 # Add the following views to the API and GraphQL
 API_EXTENSIONS = [
-    "my_plugin.api.ComponentArchitecturesViewSet",
-    "my_plugin.api.CATypeViewSet",
-    "my_plugin.api.CAInstanceViewSet",
-    "my_plugin.api.CAInstanceDeviceRelationshipViewSet",
-    "my_plugin.api.CAInstanceComponentArchitectureRelationshipViewSet",
-    "my_plugin.api.ObjectChangeLogCAInstanceViewSet",
+    "ca_instance.api.ComponentArchitecturesViewSet",
+    "ca_instance.api.CATypeViewSet",
+    "ca_instance.api.CAInstanceViewSet",
+    "ca_instance.api.CAInstanceDeviceRelationshipViewSet",
+    "ca_instance.api.CAInstanceComponentArchitectureRelationshipViewSet",
+    "ca_instance.api.ObjectChangeLogCAInstanceViewSet",
 ]
 
 # Add the following views to the API only
 API_VIEWS = [
-    "my_plugin.api.FieldChoicesAPIView",
+    "ca_instance.api.FieldChoicesAPIView",
 ]
